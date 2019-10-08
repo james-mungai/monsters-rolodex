2993254ea30173040eb43ca8a559c8889e49807e
james-mungai <jamesmungai.code@gmail.com>
2019-10-08 19:40:23 +0300

    Updates

---
+++ precache-manifest.91f4e74596121d5c7cac2d223c524425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4515c209912abd979743f6502fcd3aa4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b7a9fd16f5819053a0ff",
+    "url": "/monsters-rolodex/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "0e4df40f61ed1a94622e",
+    "url": "/monsters-rolodex/static/css/main.bc6679d8.chunk.css"
+  },
+  {
+    "revision": "b7a9fd16f5819053a0ff",
+    "url": "/monsters-rolodex/static/js/2.16cee85b.chunk.js"
+  },
+  {
+    "revision": "0e4df40f61ed1a94622e",
+    "url": "/monsters-rolodex/static/js/main.c9fa7ec7.chunk.js"
+  },
+  {
+    "revision": "6b63696966c254e95722",
+    "url": "/monsters-rolodex/static/js/runtime~main.dc35f717.js"
+  }
+]);
